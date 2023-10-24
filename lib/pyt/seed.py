f9957127d86a25a93d83d35d77f9335fa07d5664
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-24 20:01:05 +0100

    data seedings

--- lib/pyt/seed.py
+++ lib/pyt/seed.py
@@ -1,18 +1,65 @@
-import click
+from faker import Faker
+from random import choice as rc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-@click.command()
+from main import (Area, Equipment, User)
 
-@click.option('--name', '-n', default= 'Joe', help='Firstname description')
+fake = Faker()
 
 
-def main(name):
-    print('Hello World my name is {}'.format(name))
-    
-    
-    
-    
-    
-    
-if __name__  == '__main__':
-    main()
+engine = create_engine("sqlite///klinint_users.db")
+session = Session(engine, future=True)
 
+def delete_records():
+    session.query(Area).delete()
+    session.query(Equipment).delete()
+    session.query(User).delete()
+    session.commit()
+
+def create_records():
+    # Klin'iT_users
+    gender = ["M","F"]
+    position = ["Supervisor", "Team lead", "Hygiene-clerk"]
+    users = [User(
+        first_name=f'{fake.first_name()}',
+        last_name=f'{fake.last_name()}',
+        gender=rc(gender),
+        position=rc(position)
+    ) for i in range(60)]
+
+    # area or duty location 
+    duty = ["Morning", "Afternoon", "Night"]
+    location = ["Factory A", "Factory B", "Factory C", "Office", "Loading-bay", "Restroom", "Reception", "Warehouse", "Canteen", "External"]
+    areas = [Area(
+        duty = rc(duty),
+        location = rc(location),
+        user_id = fake.random_int(min=1, max=60)
+    ) for i in range(60)]
+
+    # equipment
+    equipment_types = ["Scrubbing-machine", "Vaccum-sucker", "Pressure-washer", "Telescopic-pole", "Mops", "Squee-gee", "soft-brush", "Glass-cleaner", "Drain-rods", "Truck", "Grass-Trimer", "Scaffolding", "K-rod"]
+    equipments = [Instrument(
+        type = rc(equipment_types),
+        user_id = fake.random_int(min=1, max=60)
+    ) for i in range(60)]
+
+    session.add_all(users + areas + equipments)
+    session.commit()
+    return users, areas, equipments
+
+def relate_records(users, areas, equipments):
+    for user in users:
+        users.area = rc(areas)
+        users.equipment = rc(equipments)
+    
+    session.add_all(users)
+    session.commit()
+
+if __name__ == "__main__":
+    delete_records()
+    create_records()
+
+
+    session.close()
+    session.commit()
