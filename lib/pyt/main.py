1321afa40f2b27efdaffd7f3225a6a84bd809089
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-24 19:09:11 +0100

    inputfield

--- lib/pyt/main.py
+++ lib/pyt/main.py
@@ -1,59 +1,70 @@
 from datetime import datetime
 
-from sqlalchemy import  Column, Integer, String,DateTime, create_engine
-from sqlalchemy.orm import  declarative_base, sessionmaker
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import PrimaryKeyConstraint, Column, DateTime, Integer, String, MetaData, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declarative_base()
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+Base = declarative_base(metadata=metadata)
 
 
-class UserModel(Base):
-    __tablename__ = 'user'
+class Area(Base):
+    __tablename__ = "areas"
+    __table_args__ = (PrimaryKeyConstraint("id"), )
+
+    id = Column(Integer())
+    duty = Column(String())
+    location = Column(String())
+
+    user_id = Column(Integer(), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'Id: {self.id} ' \
+        + f'Duty: {self.duty} ' \
+        + f'Location: {self.location}'
+
+
+class Equipment(Base):
+    __tablename__ = "equipments"
+    __table_args__ = (PrimaryKeyConstraint("id"), )
+
+    id = Column(Integer())
+    type = Column(String())
+
+    user_id = Column(Integer(), ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'Id: {self.id} ' \
+        + f'Type: {self.type}'
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = (PrimaryKeyConstraint("id"), )
     
-    id= Column(Integer, primary_key =True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    gender = Column(String, nullable=False)
-    position = Column(String, nullable=False)
+    id= Column(Integer())
+    first_name = Column(String())
+    last_name = Column(String())
+    gender = Column(String())
+    position = Column(String())
     created = Column(DateTime, default=datetime.utcnow)
     
-    
-    @property
-    def full_name(self):
-        return f'{self.first_name}  {self.last_name}'
-    
-    
+    equipments = relationship("Equipment", backref=backref('user'))
+    areas = relationship("Area", backref=backref('user'))
+
     def __repr__(self):
-        return(
-            f'UserModel(id={self.id}, first_name={self.first_name},'
-            f'last_name={self.last_name}, gender={self.gender},'
-            f'created={self.created}')
-            
-    
-# user = [
-#     UserModel(first_name = 'Bob', last_name='James', gender='M'),
-#     UserModel(first_name = 'Biden', last_name='Jones', gender='M'),
-#     UserModel(first_name = 'Fred', last_name='Don', gender='M'),
-#     UserModel(first_name = 'Shirley', last_name='Kim', gender='F'),
-# ]
+        return f'Id: {self.id} ' \
+        + f'First Name: {self.first_name} ' \
+        + f'Last Name: {self.last_name} ' \
+        + f'Gender: {self.gender}'\
+        + f'Position: {self.position} ' \
+        + f'Created: {self.created}'
 
-session_maker = sessionmaker(bind=create_engine('sqlite///models.db'))
-
-def create_users():
-    with session_maker() as session:
-        for user in users:
-            session.add(user)
-        session.commit()
-
-create_users()
-
-with session_maker() as session:
-    user_records = session.query(UserModel).all()
-    for user in user_records:
-        print(user)
-        
-with session_maker() as session:
-    user_records = session.query(UserModel).all
-    for user in user_records:
-        print(user.full_name)
-        
+    def test(session, last_name):
+        user = session.query(User).filter(User.last_name == last_name).first()
+        print([record for record in user])
